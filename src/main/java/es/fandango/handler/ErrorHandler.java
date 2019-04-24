da78756718dcf3965338eb74e63be941943cd0a9
RicardoFR <rfloresrosco@gmail.com>
2019-04-25 00:08:09 +0200

    Add error handler

---
+++ src/main/java/es/fandango/handler/ErrorHandler.java
@@ -0,0 +1,21 @@
+package es.fandango.handler;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import javax.inject.Singleton;
+
+/**
+ * The Generic error handler
+ */
+@Produces
+@Singleton
+@Requires(classes = {Exception.class, ExceptionHandler.class})
+public class ErrorHandler implements ExceptionHandler<Exception, HttpResponse> {
+
+  @Override public HttpResponse handle(HttpRequest request, Exception exception) {
+    return HttpResponse.notFound();
+  }
+}
