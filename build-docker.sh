08ad52cf3b60df66e07cc9f415ab1b5b64a8976e
RicardoFR <rfloresrosco@gmail.com>
2019-09-25 14:06:47 +0200

    Add docker extra config

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,8 @@ cd "$(dirname "$0")"
 ./gradlew clean shadowJar
 docker build -t vettonum/fandango:latest .
 echo
-echo
 echo "To run the docker container execute:"
 echo "$ docker run --net host vettonum/fandango:latest"
+echo
+echo "To push the generated image to the docker image repository:"
+echo "$ docker push vettonum/fandango:latest"
